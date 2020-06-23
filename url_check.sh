1b92c53e1e1a1046680837896f38cf15b81dc882
Alex Chi <iskyzh@gmail.com>
2020-06-23 23:05:03 +0800

    ci: complete url check

--- url_check.sh
+++ url_check.sh
@@ -4,4 +4,23 @@ set -e
 
 pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
-echo "Running on Pull Request #$pull_number"
+echo "running on pull request #$pull_number"
+
+echo "fetching patch file..."
+
+curl -sfL https://github.com/SJTU-CSE/awesome-cs/pull/$pull_number.patch | grep -E "^\+" | grep -Eo '(http|https)://[^)"]+' > .patch
+
+echo "checking URLs..."
+
+while IFS= read -r line
+do
+    echo "  checking $line"
+    if curl -fs "$line" > /dev/null ; then
+        echo "  ... success"
+    else
+        echo "  ... failed"
+        false
+    fi
+done < .patch
+
+echo "checking successfully"
