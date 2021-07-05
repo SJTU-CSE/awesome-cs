85ccee09b35ad40bf3d88efd0cbf9e560c2a6647
Victrid <weihau.chiang@gmail.com>
2021-07-05 23:15:39 +0800

    Change url_check to full testing

--- url_check.sh
+++ url_check.sh
@@ -4,15 +4,20 @@ set -e
 
 echo "checking URLs..."
 
+unset failed
 while IFS= read -r line
 do
-    echo "  checking $line"
     if curl -fs "$line" > /dev/null ; then
-        echo "  ... success"
+        echo -e " \033[32;1m✅\033[0m $line \033[32;1msuccess\033[0m"
     else
-        echo "  ... failed"
-        false
+        echo -e " \033[31;1m❌\033[0m $line \033[31;1mfailed\033[0m"
+        failed=1
     fi
 done
 
-echo "check successfully"
+if [ -z ${failed+x} ]; then
+    echo "check successfully"
+else 
+    echo "check failed"
+    false   
+fi
